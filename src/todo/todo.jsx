65af749cc047e1558dc2ca88ef5f6b54090dae5e
Nathalia <nathalia.alice.silv@gmail.com>
2021-03-29 22:39:42 -0300

    Consulta e Exclusão de TODOs

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -12,6 +12,13 @@ export default class Todo extends Component {
         this.state = { description: '', list: []};
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleChange(e){
@@ -21,7 +28,12 @@ export default class Todo extends Component {
     handleAdd(){
         const description = this.state.description;
         axios.post(URL, {description})
-            .then(resp => console.log("funcionou"))
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render(){
@@ -31,8 +43,10 @@ export default class Todo extends Component {
                <TodoForm 
                 description={this.state.description} 
                 handleChange={this.handleChange}
-                handleAdd={this.handleAdd}/>
-               <TodoList />
+                handleAdd={this.handleAdd}
+               />
+               <TodoList list={this.state.list}
+                 handleRemove={this.handleRemove}/>
             </div>
         )
     }
